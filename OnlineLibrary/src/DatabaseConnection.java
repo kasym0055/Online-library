ddf086086db059e2b55ed2cda1707ee53025052d
whyquas <qusaratonkachi@gmail.com>
2025-01-20 08:48:52 +0500

    Update DatabaseConnection.java

--- OnlineLibrary/src/DatabaseConnection.java
+++ OnlineLibrary/src/DatabaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/LibraryDB";
     private static final String USER = "root";
-    private static final String PASSWORD = "password"; // Замените на ваш пароль
+    private static final String PASSWORD = "password"; 
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
