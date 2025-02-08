497c35d93a279af96b164d86723723ce48a4020e
kasym0055 <78597512+kasym0055@users.noreply.github.com>
2025-02-08 11:21:29 +0500

    Delete Online-library/Online-library/OnlineLibrary/src/repository/Book directory

--- Online-library/Online-library/OnlineLibrary/src/repository/Book/IBookRepository.java
+++
@@ -1,9 +0,0 @@
-package src.repository.Book;
-
-import src.models.Book;
-
-public interface IBookRepository {
-    void create(Book book);
-    void issueBook(int bookId, int userId);
-    void viewBooks();
-}
