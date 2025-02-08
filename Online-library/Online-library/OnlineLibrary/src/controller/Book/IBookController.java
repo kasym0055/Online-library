5cdcdf0086545efc70d7d72667226e58acd94d2a
whyquas <qusaratonkachi@gmail.com>
2025-02-08 10:48:47 +0500

    Delete Online-library/Online-library/OnlineLibrary/src/controller/Book directory

--- Online-library/Online-library/OnlineLibrary/src/controller/Book/IBookController.java
+++
@@ -1,7 +0,0 @@
-package src.controller.Book;
-
-public interface IBookController {
-    void addBook(String title, String author);
-    void viewBooks();
-    void issueBook(int bookId, int userId);
-}
